58eea95de098cb2963a91aab0f8620f68d5e2dd4
xiulun <xiulun@nolovr.com>
2023-08-05 12:20:49 +0800

    基础工程

---
+++ Assets/MicroLightSDK-master/Runtime/Scripts/Contoller/MicroLightTouchpad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace MicroLight
+{
+
+    [AddComponentMenu("MicroLight/MicroLightTouchpad")]
+    [ExecuteInEditMode, RequireComponent(typeof(Transform))]
+    public class MicroLightTouchpad : ControllerInputBase
+    {
+       
+    }
+}
